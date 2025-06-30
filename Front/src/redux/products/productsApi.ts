d31c7f17b929dbdb5e6eb316a09effba7c598c63
Anna Trukshanina <annatacitus@gmail.com>
2025-06-30 06:10:08 +0400

    Product Detail Page

--- Front/src/redux/products/productsApi.ts
+++ Front/src/redux/products/productsApi.ts
@@ -1,9 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
+interface Product {
+    id: number
+    productName: string
+    productDescription: string
+    price: number
+    categoryName: string
+    stockQuantity: number
+    averageRating: number
+    reviewCount: number
+    sellerName: string
+    imageBase64: string | null
+}
+
+interface Review {
+    id: number
+    rating: number
+    comment: string
+    createdDate: string
+    userId: number
+    userName: string
+}
+
 export const productsApi = createApi({
     reducerPath: "productsApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000/api/",
+        baseUrl: "http://localhost:5278/api", // поправили порт
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("accessToken")
             if (token) {
@@ -16,6 +38,15 @@ export const productsApi = createApi({
         getSellerProducts: builder.query<any[], void>({
             query: () => "Products/seller",
         }),
+
+        getProductById: builder.query<Product, number>({
+            query: (id) => `Products/${id}`,
+        }),
+
+        getReviewsByProductId: builder.query<Review[], number>({
+            query: (productId) => `Reviews/product/${productId}`,
+        }),
+
         createProduct: builder.mutation<any, FormData>({
             query: (formData) => ({
                 url: "Products",
@@ -23,6 +54,7 @@ export const productsApi = createApi({
                 body: formData,
             }),
         }),
+
         updateProduct: builder.mutation<any, { id: number; formData: FormData }>({
             query: ({ id, formData }) => ({
                 url: `Products/${id}`,
@@ -30,6 +62,7 @@ export const productsApi = createApi({
                 body: formData,
             }),
         }),
+
         deleteProduct: builder.mutation<void, number>({
             query: (id) => ({
                 url: `Products/${id}`,
@@ -41,6 +74,8 @@ export const productsApi = createApi({
 
 export const {
     useGetSellerProductsQuery,
+    useGetProductByIdQuery,
+    useGetReviewsByProductIdQuery,
     useCreateProductMutation,
     useUpdateProductMutation,
     useDeleteProductMutation,
